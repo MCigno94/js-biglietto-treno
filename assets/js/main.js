53ab7243cf47fe31e326e8525f08343c8bda5056
MCigno94 <mattiacigno27@gmail.com>
2022-03-24 16:01:39 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,39 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 
 // Chiedere il numero di km da pecorrere
 const kmTraveled = prompt("How many km do you want to travel?");
-const yourAge = prompt("How old are you?");
 
-// impostare i limiti della variabile "kmTraveled" - nessuna stringa e nessun numero minore di 0
+// - impostare i limiti della variabile "kmTraveled" - nessuna stringa e nessun numero minore di 0
 if (kmTraveled < 0 || isNaN(kmTraveled)) {
     alert("Choice not available");
 }
 
-// impostare i limiti della variabile "yourAge" - nessuna stringa e nessun numero minore di 0
-if (yourAge < 0 || isNaN(yourAge)) {
+// Chiedere l'età del passeggero
+const yourAge = prompt("How old are you?");
+
+// - impostare i limiti della variabile "yourAge" - nessuna stringa e nessun numero minore di 0
+if (yourAge < 0 || yourAge > 100 || isNaN(yourAge)) {
     alert("Choice not available");
 }
 
-// Chiedere l'età del passeggero
+// impostare tariffa in € per il chilometraggio
+let kmRate = 0.21;
+const kmPrice = kmTraveled * kmRate;
 
-// impostare tariffa per il chilometraggio
+// - stampo il costo del viaggio in base ai km
+/* console.log("Cost of the trip based on km: " + kmPrice + "€"); */
 
 // impostare sconti in base all'età
+if (yourAge < 18) {
+    // applico uno sconto del 20%
+    const ticketPrice = kmPrice * 0.8;
+    console.log("Il costo del suo biglietto è: " + ticketPrice.toFixed(2) + "€");
+} else if (yourAge >= 65) {
+    // applico uno sconto del 40%
+    const ticketPrice = kmPrice * 0.6;
+    console.log("Il costo del suo biglietto è: " + ticketPrice.toFixed(2) + "€");
+} else {
+    console.log("Il costo del suo biglietto è: " + kmPrice.toFixed(2) + "€");
+}
+
 
 // stampare il costo del biglietto
